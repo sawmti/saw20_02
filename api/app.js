e65e4228b353b046c0ce0c7cceaf61d967807b33
vortiz86 <74000003+vortiz86@users.noreply.github.com>
2021-07-16 14:48:18 -0400

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,1 +1,22 @@
 
+const express = require('express');
+const path = require('path');
+
+const app = express()
+const root = path.resolve(__dirname, '..')
+
+// Log invocations
+app.use(function(req, res, next) { console.log(req.url); next(); });
+
+// Directly serve static content from /client
+app.use(express.static(root + '/client'));
+
+// Simple REST API that returns some entities
+app.get('/api/entities', (req,res) => 
+ res.send({ entities: 
+   ['Q2887', 
+    'Q33986'
+   ]})
+);
+
+module.exports = app
